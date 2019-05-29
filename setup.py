1f5aba9212bd48a66ffc21626550f5af869a38d1
Chris Capurso <chrisc@stitchdata.com>
2019-05-29 16:45:10 +0000

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-oracle',
-      version='0.1.0',
+      version='0.2.0',
       description='Singer.io tap for extracting data from Oracle',
       author='Stitch',
       url='https://singer.io',
